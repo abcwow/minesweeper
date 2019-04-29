53aea8f52da8054e0f44037c95067e54f8b7110c
abcwow <abcwow@yeah.net>
2019-04-29 20:35:49 +0800

    1.0.1 to be optimized

--- mouseMove.go
+++ mouseMove.go
@@ -4,22 +4,22 @@ import (
 	"github.com/go-vgo/robotgo"
 )
 
-func MoveAndClick(startX, startY int, grid *[30][16]byte) {
+func MoveAndClick(startX, startY int, grid []byte) {
 
-	for i := 0; i < 30; i++ {
-		for j := 0; j < 16; j++ {
-			if grid[i][j] == 31 {
+	for i := 0; i < 16; i++ {
+		for j := 0; j < 30; j++ {
+			if grid[i*30+j] == 31 {
 
-				ex := startX + j*16
-				ey := startY + i*16
+				ex := startX + j*16 + 8
+				ey := startY + i*16 + 8
 				robotgo.MoveMouseSmooth(ex, ey, 1.0, 3.0)
 				robotgo.MouseClick("right", true)
 
 			}
-			if grid[i][j] == 30 {
+			if grid[i*30+j] == 30 {
 
-				ex := startX + j*16
-				ey := startY + i*16
+				ex := startX + j*16 + 8
+				ey := startY + i*16 + 8
 				robotgo.MoveMouseSmooth(ex, ey, 1.0, 3.0)
 				robotgo.MouseClick("left", true)
 			}
