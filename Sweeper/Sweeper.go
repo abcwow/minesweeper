74c2b072520d8f8e2719daf2a9f2172ae316e2e9
zjc <zjc@gsk.com>
2019-04-30 08:55:59 +0800

    fix bug

--- Sweeper/Sweeper.go
+++ Sweeper/Sweeper.go
@@ -1,11 +1,19 @@
 package sweeper
 
 import "fmt"
+import "time"
+import "math/rand"
 
 type unit struct {
-	state   uint8
+	state uint8
+
 	besides []int
 }
+
+const EMPYTUNIT byte = 10
+const SAFEUNIT byte = 30
+const SWEEPUNIT byte = 31
+const SWEEPDIDUNIT byte = 0xff
 
 func getBesideIndex(n int) []int {
 	var dat [8]int
@@ -58,26 +66,26 @@ func getBesideIndex(n int) []int {
 		dat[3] = n + 29
 		dat[4] = n + 30
 		return dat[:5]
+	} else {
+		dat[0] = n - 31
+		dat[1] = n - 30
+		dat[2] = n - 29
+		dat[3] = n - 1
+		dat[4] = n + 1
+		dat[5] = n + 29
+		dat[6] = n + 30
+		dat[7] = n + 31
+		return dat[:8]
 	}
-	dat[0] = n - 31
-	dat[1] = n - 30
-	dat[2] = n - 29
-	dat[3] = n - 1
-	dat[4] = n + 1
-	dat[5] = n + 29
-	dat[6] = n + 30
-	dat[7] = n + 31
-	return dat[:8]
 }
 
 func getBesideState(dat []byte, index []int) []byte {
 	var state []byte
 
 	for i := 0; i < len(index); i++ {
-		if dat[index[i]] != 10 {
+		if dat[index[i]] != EMPYTUNIT {
 			state = append(state, (byte)(dat[index[i]]))
 		}
-
 	}
 	return state
 }
@@ -96,21 +104,25 @@ func getEmptyIndex(dat []byte, index []int) []int {
 func getSweeperNum(dat []byte) byte {
 	var n byte = 0
 	for i := 0; i < len(dat); i++ {
-		if dat[i] == 31 {
+		if dat[i] == SWEEPUNIT {
 			n++
 		}
 	}
 	return n
 }
 
-func GetSweeper(dat []byte) []byte {
+func getRandSweep() int {
+	return rand.Intn(480)
+}
 
+func GetSweeper(dat []byte) []byte {
 	var update bool = true
+	var cnt int = 0
 
 	for update == true {
 		update = false
 		for i := 0; i < 480; i++ {
-			if dat[i] == 10 {
+			if dat[i] == EMPYTUNIT || dat[i] == SWEEPDIDUNIT {
 				continue
 			}
 			besides := getBesideIndex(i)
@@ -121,19 +133,26 @@ func GetSweeper(dat []byte) []byte {
 
 			if dat[i] == (byte)(len(emptySt))+n {
 				for j := 0; j < len(emptySt); j++ {
-					dat[emptySt[j]] = 31
+					dat[emptySt[j]] = SWEEPUNIT
 					update = true
+					cnt++
 				}
 			}
 			if dat[i] == n && dat[i] != 0 && dat[i] != 31 {
 				for j := 0; j < len(emptySt); j++ {
-					dat[emptySt[j]] = 30
+					dat[emptySt[j]] = SAFEUNIT
 					update = true
+					cnt++
 				}
 			}
 		}
 	}
-
+	if cnt == 0 {
+		index := getRandSweep()
+		if dat[index] != SWEEPUNIT && dat[index] == 0 {
+			dat[index] = SAFEUNIT
+		}
+	}
 	return dat
 }
 
@@ -142,6 +161,7 @@ type SweeperMap struct {
 }
 
 func SweeperCreateMap() SweeperMap {
+	rand.Seed(time.Now().Unix())
 	sm := SweeperMap{}
 	for i := 0; i < 480; i++ {
 		sm.unit[i].besides = getBesideIndex(i)
